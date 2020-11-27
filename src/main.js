bc4a8ba3167be39c4082bdab5a9d88ebc32023e4
wangyalou <17691090174@sina.cn>
2020-11-27 22:47:31 +0800

    vue-pc

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue';
+import App from './App';
+
+import router from './router';
+import "./styles/reset.css"
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+  router
+}).$mount('#app')
